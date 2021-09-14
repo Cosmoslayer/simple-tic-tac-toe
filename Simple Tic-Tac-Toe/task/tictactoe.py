6a26989a43f062bc7fa4057917b11f38454beafa
Mark Stephen Gonzales <cosmoslayer@gmail.com>
2021-09-14 21:53:39 +0800

    Finish Stage 2/5

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,4 +1,7 @@
 # write your code here
-print("X O X")
-print("O X O")
-print("X X O")
+cells = list(input("Enter cells: "))
+print("---------")
+print(f"| {cells[0]} {cells[1]} {cells[2]} |")
+print(f"| {cells[3]} {cells[4]} {cells[5]} |")
+print(f"| {cells[6]} {cells[7]} {cells[8]} |")
+print("---------")
